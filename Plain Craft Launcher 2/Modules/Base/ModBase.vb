84230e5f971c5acafc96a65d490540e696973f3b
tsxc <tsxc_personal@163.com>
2025-06-07 16:35:53 +0800

    init repo feat: 关闭社区版提示

--- Plain Craft Launcher 2/Modules/Base/ModBase.vb
+++ Plain Craft Launcher 2/Modules/Base/ModBase.vb
@@ -2888,7 +2888,7 @@ NextElement:
         If ForceOpenLog OrElse (ShowMsgbox AndAlso MyMsgBox("若你在汇报一个 Bug，请点击 打开文件夹 按钮，并上传 Launch-" & currentDate & "-[一串数字].log 中包含错误信息的文件。" & vbCrLf & "游戏崩溃一般与启动器无关，请不要因为游戏崩溃而提交反馈。", "反馈提交提醒", "打开文件夹", "不需要") = 1) Then
             OpenExplorer(ExePath & "PCL\Log\")
         End If
-        OpenWebsite("https://github.com/PCL-Community/PCL2-CE/issues/")
+        OpenWebsite("https://github.com/tsxc-github/PCL2-CE/issues/")
     End Sub
     Public Function CanFeedback(ShowHint As Boolean) As Boolean
         If Not IsVerisonLatest() Then
