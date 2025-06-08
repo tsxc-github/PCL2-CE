786994adaed83d46bcc9a9672983fe94536be776
tsxc <tsxc_personal@163.com>
2025-06-08 21:20:01 +0800

    v0.0.1-beta.1

--- Plain Craft Launcher 2/Modules/Base/ModBase.vb
+++ Plain Craft Launcher 2/Modules/Base/ModBase.vb
@@ -14,13 +14,13 @@ Public Module ModBase
 
 #Region "声明"
 
-    '下列版本信息由更新器自动修改
-    Public Const VersionBaseName As String = "2.11.2-beta.3" '不含分支前缀的显示用版本名
-    Public Const VersionStandardCode As String = "2.11.1." & VersionBranchCode '标准格式的四段式版本号
+    '下列版本信息由能工智人自动修改
+    Public Const VersionBaseName As String = "0.0.1-beta.1" '不含分支前缀的显示用版本名
+    Public Const VersionStandardCode As String = "0.0.1." & VersionBranchCode '标准格式的四段式版本号
     Public Const CommitHash As String = "native" 'Commit Hash，由 GitHub Workflow 自动替换
     Public CommitHashShort As String = If(CommitHash = "native", "native", CommitHash.Substring(0, 7)) 'Commit Hash，取前 7 位
-    Public Const UpstreamVersion As String = "2.10.0" '上游版本
-    Public Const VersionCode As Integer = 373 '内部版本号
+    Public Const UpstreamVersion As String = "2.11.2-beta.3" '上游版本
+    Public Const VersionCode As Integer = 0 '内部版本号
     '自动生成的版本信息
 #If RELEASE Then
     Public Const VersionBranchName As String = "Slow Ring"
