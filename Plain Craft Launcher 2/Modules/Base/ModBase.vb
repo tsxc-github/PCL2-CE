5722e9ef6d15117e08cf79baf6615b309a5f444c
tsxc <tsxc_personal@163.com>
2025-06-13 20:35:05 +0800

    v0.0.3

--- Plain Craft Launcher 2/Modules/Base/ModBase.vb
+++ Plain Craft Launcher 2/Modules/Base/ModBase.vb
@@ -15,12 +15,12 @@ Public Module ModBase
 #Region "声明"
 
     '下列版本信息由能工智人自动修改
-    Public Const VersionBaseName As String = "v0.0.2" '不含分支前缀的显示用版本名
-    Public Const VersionStandardCode As String = "0.0.2." & VersionBranchCode '标准格式的四段式版本号
+    Public Const VersionBaseName As String = "v0.0.3" '不含分支前缀的显示用版本名
+    Public Const VersionStandardCode As String = "0.0.3." & VersionBranchCode '标准格式的四段式版本号
     Public Const CommitHash As String = "native" 'Commit Hash，由 GitHub Workflow 自动替换
     Public CommitHashShort As String = If(CommitHash = "native", "native", CommitHash.Substring(0, 7)) 'Commit Hash，取前 7 位
     Public Const UpstreamVersion As String = "2.11.5" '上游版本
-    Public Const VersionCode As Integer = 4 '内部版本号
+    Public Const VersionCode As Integer = 5 '内部版本号
     '自动生成的版本信息
 #If DEBUG Then
     Public Const VersionBranchName As String = "Debug"
